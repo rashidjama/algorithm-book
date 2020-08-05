2a0a17c9e3e0158564791a51b2839a922a5df23a
Rashid Jama <rashidjama5@gmail.com>
2020-08-04 18:45:42 -0700

    final commit for array1 solutions

--- arrays.js
+++ arrays.js
@@ -31,21 +31,37 @@ function instertAt(arr, idx, value) {
   }
   return arr
 }
-console.log(instertAt([4,5,6,7], 2, 9))
+// console.log(instertAt([4,5,6,7], 2, 0))
 
 
 
 // Remove At
 // Given an array and an index into array, remove and return the array value at that index. Do this without using built-in array methods except pop(). Think of popFront(arr) as equivalent to removeAt(arr,0).
+function removeAt(arr, value) {
+  return arr[value]
+}
+// console.log(removeAt([1,2,3,4], 1))
 
 
 
 // Swap Pairs
 // Swap positions of successive pairs of values of given array. If length is odd, do not change the final element. For [1,2,3,4], return [2,1,4,3]. For example, change input ["Brendan",true,42] to [true,"Brendan",42]. As with all array challenges, do this without using any built-in array methods.
-
-
+function swipePairs(arr) {
+  return [arr[0], arr[1], arr[2], arr[3]] = [arr[1], arr[0], arr[3], arr[2]];
+}
+// console.log(swipePairs([1,2,3,4]))
 
 // Remove Duplicates
 // Sara is looking to hire an awesome web developer and has received applications from various sources. Her assistant alphabetized them but noticed some duplicates. Given a sorted array, remove duplicate values. Because array elements are already in order, all duplicate values will be grouped together. As with all these array challenges, do this without using any built-in array methods.
+function removeDuplicates(arr){
+  let newArray = []
+  for(let i=0; i<arr.length;i++) {
+    if(arr[i] !== arr[i+1]) {
+      newArray.push(arr[i])
+    }
+  }
+  return newArray
+}
+console.log(removeDuplicates([1,2,2,3,5,5]))
 
 // Second: Solve this without using any nested loops.
